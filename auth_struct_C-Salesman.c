a1f5c95f91e0eb127b90405a2074009e8ac3c6de
Evangelos Mamalakis <mamalakis@auth.gr>
2014-06-01 06:12:34 +0300

    Update auth_struct_C-Salesman.c
    Indentation

--- auth_struct_C-Salesman.c
+++ auth_struct_C-Salesman.c
@@ -20,67 +20,67 @@ int minDistanceTravelled = INT_MAX, minOrigin;
 int Route[ MAXN ], NStops = 0;
 
 void inputGraph() {
-	scanf( "%d %d", &NCities, &NRoads );
-	assert( NCities < MAXN 
-	     && NRoads  < pow( MAXN, 2 ) );
-	assert( NRoads == NCities * (NCities - 1) / 2 ) // Complete Graph
-	FOR0( i, NRoads ) { 
-		static int city1, city2, length;
-		scanf( "%d %d %d", &city1, &city2, &length );
-		assert( city1      < NCities 
-		     && city2 < NCities
-		     && length > 0 );
-		distance[ city1 ][ city2 ] = length;
-		distance[ city2 ][ city1 ] = length;
-	}
-	FOR0( sameOriginDestination, NCities ) {
-		distance[ sameOriginDestination ][ sameOriginDestination ] = 0;
-	}
+    scanf( "%d %d", &NCities, &NRoads );
+    assert( NCities < MAXN
+         && NRoads  < pow( MAXN, 2 ) );
+    assert( NRoads == NCities * (NCities - 1) / 2 ) // Complete Graph
+    FOR0( i, NRoads ) {
+        static int city1, city2, length;
+        scanf( "%d %d %d", &city1, &city2, &length );
+        assert( city1 < NCities
+             && city2 < NCities
+             && length > 0 );
+        distance[ city1 ][ city2 ] = length;
+        distance[ city2 ][ city1 ] = length;
+    }
+    FOR0( sameOriginDestination, NCities ) {
+        distance[ sameOriginDestination ][ sameOriginDestination ] = 0;
+    }
 }
 int findNearestNeighbour( int origin ) {
-	int nearest = UNDEFINED;
-	FOR0( neighbor, NCities ) {
-		if ( !visited[ neighbor ] &&
-	       ( nearest == UNDEFINED || distance[ origin ][ neighbor ] < distance[ origin ][ nearest ] ) ) {
-			nearest = neighbor;
-		}
-	}
-	return nearest;
+    int nearest = UNDEFINED;
+    FOR0( neighbor, NCities ) {
+        if ( !visited[ neighbor ] &&
+           ( nearest == UNDEFINED || distance[ origin ][ neighbor ] < distance[ origin ][ nearest ] ) ) {
+            nearest = neighbor;
+        }
+    }
+    return nearest;
 }
 int travelFrom( int origin, BOOL saveRoute ) {
-	FOR0( city, NCities ) { 
-		visited[ city ] = FALSE;
-	}
-	int current = origin, nearest = origin, distanceTravelled = 0;
-	while( nearest != UNDEFINED ) {
-		visited[ nearest ] = TRUE;
-		distanceTravelled += distance[ current ][ nearest ];
-		if ( saveRoute ) {
-			Route[ NStops++ ] = nearest;
-		}
-		current = nearest;
-		nearest = findNearestNeighbour( current );
-	} 	
-	return distanceTravelled;
+    FOR0( city, NCities ) {
+        visited[ city ] = FALSE;
+    }
+    int current = origin, nearest = origin, distanceTravelled = 0;
+    while( nearest != UNDEFINED ) {
+        visited[ nearest ] = TRUE;
+        distanceTravelled += distance[ current ][ nearest ];
+        if ( saveRoute ) {
+            Route[ NStops++ ] = nearest;
+        }
+        current = nearest;
+        nearest = findNearestNeighbour( current );
+    }
+    return distanceTravelled;
 }
 void findMinDistanceTravelled() {
-	FOR0( origin, NCities ) {
-		int distanceTravelled = travelFrom( origin, FALSE );
-		if( distanceTravelled < minDistanceTravelled ) {
-			minDistanceTravelled = distanceTravelled;
-			minOrigin = origin;
-		}
-	}
+    FOR0( origin, NCities ) {
+        int distanceTravelled = travelFrom( origin, FALSE );
+        if( distanceTravelled < minDistanceTravelled ) {
+            minDistanceTravelled = distanceTravelled;
+            minOrigin = origin;
+        }
+    }
 }
 int main() {
-	inputGraph();
-	findMinDistanceTravelled();
-	printf( "Travell %d from %d\n", minDistanceTravelled, minOrigin );
-	travelFrom( minOrigin, TRUE );
-	FOR0( stop, NStops ) {
-		printf( "%d ", Route[ stop ] );
-	}
-	return 0;
+    inputGraph();
+    findMinDistanceTravelled();
+    printf( "Travell %d from %d\n", minDistanceTravelled, minOrigin );
+    travelFrom( minOrigin, TRUE );
+    FOR0( stop, NStops ) {
+        printf( "%d ", Route[ stop ] );
+    }
+    return 0;
 }
 /*
 Σε ένα πλήρες γράφημα με N κορυφές η κάθε κορυφή του συνδέεται με όλες τις άλλες με ακμές  η κάθε μια από τις οποίες έχει ένα ορισμένο βάρος. Στο γράφημα αυτό ζητείται να βρεθεί η διαδρομή η οποία περνά, μια φορά, από όλες τις κορυφές του γραφήματος και το άθροισμα των βαρών των ακμών από τις οποίες περνά να είναι ελάχιστο (Πρόβλημα του περιοδεύοντας εμπόρου).
