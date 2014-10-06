3cf8d208029f4ee7687e1054355ee54847fad37b
Evangelos Mamalakis <mamalakis@auth.gr>
2014-10-06 06:56:19 +0300

    Add "Consecutive Numbers in Array"

--- decode/sfhmmy/Consecutive Numbers in Array/consecutive.py
+++ decode/sfhmmy/Consecutive Numbers in Array/consecutive.py
@@ -1,4 +1,6 @@
-##sys.stdin = open('input.txt', 'r')
+import sys
+sys.stdin = open('Test_0_s_0.in')
+sys.stdout = open('Test_0_s_0.out.txt', 'w')
 
 def getInts():
     return list(map(int, input().split()))
@@ -6,25 +8,26 @@ def getInts():
 def cons(array):
     count = 1
     maximum = 1
+    
     for i in range(1, len(array)):
         if array[i] == array[i-1] + 1:
             count = count + 1
-            maximum = max(maximum, count)
         else:
+            maximum = max(maximum, count)
             count = 1
-    return maximum
+            
+    return max(maximum, count)
 
 if __name__ == "__main__":
     length, queries = getInts()
 
     array = getInts()
     print("Initial array: {}".format(cons(array)))
-    
-    for query in range(1, queries+1):
-        x, y = getInts()
+
+    for query, line in enumerate(sys.stdin, 1):
+        x, y = map(int, line.split())
         array[x] = y
         print("Change #{}: {}".format(query, cons(array)))
 
-    
-
+    sys.stdout.flush()
     
