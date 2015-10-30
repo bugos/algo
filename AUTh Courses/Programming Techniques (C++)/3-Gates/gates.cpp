be238418e87b8d1fac96821ac896bdca2d6fa663
Evangelos Mamalakis <baggos12@gmail.com>
2015-10-30 22:32:29 +0200

    Update gates.cpp
    as submitted 
    φελούς πετώ στη θάλασσα
    κι αυτοί στον πάτο πάνε
    κι άλλοι πετούνε σίδερα
    και στον αδρό κυλάνε

--- AUTh Courses/Programming Techniques (C++)/3-Gates/gates.cpp
+++ AUTh Courses/Programming Techniques (C++)/3-Gates/gates.cpp
@@ -1,26 +1,36 @@
 #include <iostream>
 using namespace std;
 
+#define BASE 2
+#define MAXINPUTS 10
 class Gate {
 protected:
 	int nInputs;
-	bool input1, input2;
+	bool input[MAXINPUTS];
 public:
-	void setInput1(bool v) {
-		input1 = v;
-	}
-	void setInput2(bool v) {
-		input2 = v;
-	}
 	virtual bool getOutput() = 0;
 	const char* name;
 	
+	Gate( int nInputs, const char* name ) : nInputs(nInputs), name(name) {
+		for ( int i = 0; i < nInputs; i++ ) {
+			setInput( i, 0 ); // obsolete?
+		}
+	}
+	void setInput( int i, bool v ) {
+		if ( i >= MAXINPUTS ) return;
+		input[ i ] = v;
+	}
+	void setInput( bool v ) {
+		setInput( 0, v );
+	}
 	void truthTable() {
-		for ( int i = 0; i < 2 ; i++ ) {
-			setInput1(i);
+		for ( int i = 0; i < BASE ; i++ ) {
+			setInput( 0, i );
+			
 			if (nInputs > 1) {
-				for ( int j = 0; j < 2; j++ ) {
-					setInput2(j);
+				for ( int j = 0; j < BASE; j++ ) {
+					setInput( 1, j );
+					
 					cout << i << name << j << "=" << getOutput() << '\n';
 				}
 			}
@@ -33,32 +43,62 @@ public:
 
 class AndGate : public Gate {
 public:
-	AndGate() {
-		name = "AND";
-		nInputs = 2;
+	AndGate( bool input1 = 0, bool input2 = 0 ) : Gate(2, "AND") {
+		setInput( 0, input1 );
+		setInput( 1, input2 );
 	}
 	bool getOutput() {
-		return input1 && input2;
+		return input[0] && input[1];
 	}
 };
 class OrGate : public Gate {
 public:
-	OrGate() {
-		name = "OR";
-		nInputs = 2;
+	OrGate( bool input1 = 0, bool input2 = 0 ) : Gate(2, "OR") {
+		setInput( 0, input1 );
+		setInput( 1, input2 );
 	}
 	bool getOutput() {
-		return input1 || input2;
+		return input[0] || input[1];
 	}
 };
 class NotGate : public Gate {
 public:
-	NotGate() {
-		name = "NOT";
-		nInputs = 1;
+	NotGate( bool input1 = 0 ) : Gate( 1, "NOT" ) {
+		setInput( input1 );
 	}
 	bool getOutput() {
-		return !input1;
+		return !input[0];
+	}
+};
+
+class HalfAdder : public Gate {
+public:
+	HalfAdder( bool input1 = 0, bool input2 = 0 ) : Gate( 2, "HALFADDER" ) {
+		setInput( 0, input1 );
+		setInput( 1, input2 );
+	}
+	bool getSum() {
+		OrGate og( input[0], input[1] );
+		bool orResult = og.getOutput();
+
+		AndGate ag( input[0], input[1] );
+		NotGate ng( ag.getOutput() ); 
+		bool nandResult = ng.getOutput();
+
+		AndGate ag2( orResult, nandResult );
+		bool xorResult = ag2.getOutput();
+		
+		return xorResult;
+	}
+	bool getCarry() {
+		AndGate ag(input[0], input[1]);
+		bool andResult = ag.getOutput();
+		
+		return andResult;
+	}
+	bool getOutput() {
+		return getSum();
+		//return getCarry();
 	}
 };
 
@@ -69,5 +109,9 @@ int main() {
 	ag.truthTable();
 	og.truthTable();
 	ng.truthTable();
+	
+	HalfAdder ha;
+	ha.truthTable();
+	
 	return 0;
 }
