b8275e0eadca5f9a022b8ef9e6daa4038a0f7365
Evangelos Mamalakis <mamalakis@auth.gr>
2015-11-10 17:06:52 +0200

    Create normalizeDate()
    normalisation not working properly

--- AUTh Courses/Programming Techniques (C++)/4-Calendar/calendar.cpp
+++ AUTh Courses/Programming Techniques (C++)/4-Calendar/calendar.cpp
@@ -1,27 +1,57 @@
+// a4-calendar.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
 #include <iostream>
 using namespace std;
 
+#define DAYS_IN_MONTH 31
 class Date {
+private:
     int *day, *month, *year;
-
-public:
-    Date( int d=0, int m=0, int y=0 ) {
+    void allocateMemory() {
         day = new int;
         month = new int;
         year = new int;
+    }
+    void normalizeNumber(int &remainder, int max, int &quotientDestination, bool forcePositive = 1 ) {
+        if (forcePositive) { remainder -= max + 1; } // Take a month in case of negative date
+
+        int remainderSign = remainder > 0 ? 1 : -1;
+        quotientDestination += ( remainder - remainderSign) / max;
+        remainder = ( remainder - remainderSign) % max + remainderSign;
+
+        if (forcePositive) { remainder += max + 1; } // Return subtracted days.
+    }
+    void normalizeDate() {
+        normalizeNumber( *day, DAYS_IN_MONTH, *month );
+        normalizeNumber( *month, 12, *year );
+    }
+
+public:
+    // Getters
+    int getDay() { return *day; }
+    int getMonth() { return *month; }
+    int getYear() { return *year; }
+
+    Date(int d = 0, int m = 0, int y = 0) {
+        allocateMemory();
+
         *day = d;
         *month = m;
         *year = y;
-        
+
+        normalizeDate();
     }
-    Date( const Date &that ) {
+    Date(const Date &that) {
         Date(*(that.day), *(that.month), *(that.year));
     }
-    Date& operator=(const Date& that){
+    Date& operator=(const Date& that) {
         *day = *(that.day);
         *month = *(that.month);
         *year = *(that.year);
-        
+
         return *this;
     }
     ~Date() {
@@ -33,25 +63,23 @@ public:
         os << *(date.day) << '/' << *(date.month) << '/' << *(date.year);
         return os;
     }
+
     Date operator+(const Date& that) {
-        Date res;
-        int newDay = *day + *(that.day);
-        *(res.month) += newDay / 31; //alou
-        *(res.day) = newDay % 31 + 1;
-        
-        int newMonth = *month + *(that.month);
-        *(res.year) += newMonth / 12; //alou
-        *(res.month) += newMonth % 12 + 1;
-        
-        *(res.year) += *year + *(that.year);
-        
-        return res;
+        return Date(*day + *(that.day), *month + *(that.month), *year + *(that.year));
+    }
+    Date operator-(const Date& that) {
+        return Date(*day - *(that.day), *month - *(that.month), *year - *(that.year));
     }
 };
 
 int main() {
-    Date bday(31,5,1995);
-    Date a(1,7,0);
-    cout << bday+a;
+    Date bday(31, 5, 1995);
+    Date a(1, 7, 0);
+    Date b(-31, 3, 0);
+    //Date b(a);
+    //b = bday;
+    cout << bday + a << ' ' << a << ' ' << bday << ' ' << b;
+    int r;
+    cin >>r ;
     return 0;
 }
