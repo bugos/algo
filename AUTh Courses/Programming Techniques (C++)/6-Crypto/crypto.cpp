7650993f3e0942e208a62496ecb25b5c908a74fb
Evangelos Mamalakis <mamalakis@auth.gr>
2015-11-22 16:10:06 +0200

    Implement crypto task

--- AUTh Courses/Programming Techniques (C++)/6-Crypto/crypto.cpp
+++ AUTh Courses/Programming Techniques (C++)/6-Crypto/crypto.cpp
@@ -1,27 +1,65 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Crypto {
 protected:
-	string Message;
-	string Encrypted;
-	string Decrypted;
+	string message;
+	string encrypted;
+	string decrypted;
 public:
-	string virtual Encryption ()=0;
-	string virtual Decryption ()=0;
-	Crypto(string msg);
-	string getMessage();
-	void setMessage();
-	string getEncrypted();
-	string getDecrypted();
+	void virtual Encryption() = 0;
+	void virtual Decryption() = 0;
+	Crypto( const string msg ) : message( msg ) {}
+	string getMessage() {
+		return message;
+	}
+	void setMessage( string message ) {
+		this->message = message;
+	}
+	string getEncrypted() {
+		return encrypted;
+	}
+	string getDecrypted() {
+		return decrypted;
+	}
 };
 
 class Cesar: public Crypto {
-
+	static int const key = 1;
+public:
+	Cesar( const string msg ) : Crypto(msg) {}
+	void virtual Encryption() {
+		encrypted.resize( message.length() );
+		for ( int i = 0; i < message.length(); i++ ) {
+			encrypted[i] = message[i] + key;
+		}
+	}
+	void virtual Decryption() {
+		decrypted.resize( encrypted.length() );
+		for ( int i = 0; i < encrypted.length(); i++ ) {
+			decrypted[i] = encrypted[i] - key;
+		}
+	}
 };
 
 class XOR: public Crypto {
-
+//	static char const key = 'k';
+public:
+	char key;
+	XOR( const string msg ) : Crypto(msg), key('k') {}
+	void virtual Encryption() {
+		encrypted.resize( message.length() );
+		for ( int i = 0; i < message.length(); i++ ) {
+			encrypted[i] = (char)message[i] ^ key;
+		}
+	}
+	void virtual Decryption() {
+		decrypted.resize( encrypted.length() );
+		for ( int i = 0; i < encrypted.length(); i++ ) {
+			decrypted[i] = encrypted[i] ^ key;
+		}
+	}
 };
 
 
@@ -30,13 +68,16 @@ int main() {
 	c_enc.Encryption();
 	string resEncCes=c_enc.getEncrypted();
 	cout << resEncCes << endl;
+
 	c_enc.Decryption();
-	cout << c_end.getDecrypted();
+	cout << c_enc.getDecrypted() << endl;
+
 	XOR x_enc("hello");
 	x_enc.Encryption();
-	string resEncXOR = c_enc.getEncrypted();
+	string resEncXOR = x_enc.getEncrypted();
 	cout << resEncXOR << endl;
+
 	x_enc.Decryption();
-	cout << x_end.getDecrypted();
+	cout << x_enc.getDecrypted() << endl;
 
 }
