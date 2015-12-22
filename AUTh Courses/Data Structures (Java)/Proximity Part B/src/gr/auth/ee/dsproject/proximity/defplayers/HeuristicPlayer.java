2bf3a5637701502496a8a615356cb0d0c0ac870e
Evangelos Mamalakis <mamalakis@auth.gr>
2015-12-23 00:34:46 +0200

    First logic for ds proximity player

--- AUTh Courses/Data Structures (Java)/Proximity Part B/src/gr/auth/ee/dsproject/proximity/defplayers/HeuristicPlayer.java
+++ AUTh Courses/Data Structures (Java)/Proximity Part B/src/gr/auth/ee/dsproject/proximity/defplayers/HeuristicPlayer.java
@@ -1,9 +1,12 @@
 package gr.auth.ee.dsproject.proximity.defplayers;
+import java.util.PriorityQueue;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Collections;
+
 
 import gr.auth.ee.dsproject.proximity.board.Board;
 import gr.auth.ee.dsproject.proximity.board.ProximityUtilities;
@@ -63,14 +66,66 @@ public class HeuristicPlayer implements AbstractPlayer {
 
 	}
 
-	public int[] getNextMove (Board board , int randomNumber)
-	{
-		//TODO fill this function
+	public int[] getNextMove (Board board , int randomNumber) {
+		System.out.println(randomNumber);
+		ArrayList<double[]> possibleMoves = new ArrayList<double[]>();
+		
+		for ( int x = 0; x < ProximityUtilities.NUMBER_OF_COLUMNS; x++ ) {
+			for ( int y = 0; y < ProximityUtilities.NUMBER_OF_ROWS; y++ ) {
+				if ( !isTaken(board, x, y) ) {
+					Tile t = board.getTile(x,y);
+					Double e = getEvaluation(board, randomNumber, t);
+					possibleMoves.add( new double[] { x, y, e } );
+				}
+			}
+		}
+		
+		double[] bestMove = Collections.max(possibleMoves, new Comparator<double[]>() {
+		    public int compare(double[] move1, double[] move2) {
+		    	return (int)(move1[2] - move2[2]);
+		    }
+		});
+		
+		return new int[] {(int)bestMove[0], (int)bestMove[1], randomNumber};
 	}
 	
-	double getEvaluation(Board board, int randomNumber, Tile tile){
-		//TODO fill this function
+	private boolean isTaken(Board board, int x, int y) {
+		return board.getTile(x, y).getColor() != 0;
+	}
+	
+	double getEvaluation(Board board, int randomNumber, Tile tile) {
+		Tile[] neighbors = ProximityUtilities.getNeighbors(tile.getX(), tile.getY(), board);
+		int neighborsScore = 0;
+		int covered = 0;
 		
+		for (Tile neighbor:neighbors) {
+			if ( neighbor == null) { // out of board
+				covered += tile.getScore();
+			}
+			else if ( neighbor.getPlayerId() ==  0 ) { // empty neighbor
+				
+			}
+			else if ( neighbor.getPlayerId() == id ) { // my neighbor
+				neighborsScore += 1;
+				covered += neighbor.getScore();
+				
+				covered += tile.getScore();
+			}
+			else { // enemy neighbor
+				if ( randomNumber > neighbor.getScore() ) {
+					neighborsScore += neighbor.getScore();
+					covered += neighbor.getScore();
+				}
+				covered += tile.getScore();
+			}
+		}
+		
+		System.out.println(covered / 6. + " " + neighborsScore);
+		return neighborsScore + (covered / 6.);
+		// points gained
+		// safety points gained
 	}
 
 }
+
+
