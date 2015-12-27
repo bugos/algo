e1dc14c7078b5ed4dde29eaedea1c2004a8f89bd
Evangelos Mamalakis <mamalakis@auth.gr>
2015-12-27 22:22:09 +0200

    proximity task 2 preview
    finalised features, start writing report
    1 hour, 30 minutes left

--- AUTh Courses/Data Structures (Java)/Proximity Part B/src/gr/auth/ee/dsproject/proximity/defplayers/SecondHeuristicPlayer.java
+++ AUTh Courses/Data Structures (Java)/Proximity Part B/src/gr/auth/ee/dsproject/proximity/defplayers/SecondHeuristicPlayer.java
@@ -67,9 +67,9 @@ public class SecondHeuristicPlayer implements AbstractPlayer {
 	}
 
 	public int[] getNextMove (Board board , int randomNumber) {
-		System.out.println(randomNumber);
 		ArrayList<double[]> possibleMoves = new ArrayList<double[]>();
 		
+		// Get all empty tiles as possible moves
 		for ( int x = 0; x < ProximityUtilities.NUMBER_OF_COLUMNS; x++ ) {
 			for ( int y = 0; y < ProximityUtilities.NUMBER_OF_ROWS; y++ ) {
 				if ( !isTaken(board, x, y) ) {
@@ -80,6 +80,7 @@ public class SecondHeuristicPlayer implements AbstractPlayer {
 			}
 		}
 		
+		// Get the move with the maximum evaluation
 		double[] bestMove = Collections.max(possibleMoves, new Comparator<double[]>() {
 		    public int compare(double[] move1, double[] move2) {
 		    	return (int)(move1[2] - move2[2]);
@@ -89,69 +90,106 @@ public class SecondHeuristicPlayer implements AbstractPlayer {
 		return new int[] {(int)bestMove[0], (int)bestMove[1], randomNumber};
 	}
 	
+	/**
+	 * Specify if the given Tile is taken
+	 * @param board
+	 * @param x
+	 * @param y
+	 * @return Boolean true if Tile taken, else false
+	 */
 	private boolean isTaken(Board board, int x, int y) {
 		return board.getTile(x, y).getColor() != 0;
 	}
 	
-	protected double getEvaluation(Board board, int randomNumber, Tile tile) {
-		Tile[] neighbors = ProximityUtilities.getNeighbors(tile.getX(), tile.getY(), board);
-		int neighborsScore = 0;
-		int covered = 0;
-		double enemy = 0;
-		int pid = (id == 1) ? 2 : 1;
-		double enemyHere = getScoreEvaluation( board,  10, tile, pid) - 10;
+	double getEvaluation(Board board, int randomNumber, Tile tile) {
+		double scoreGain = 0; // points gained after this move
+		double coveredGain = 0; // vulnerability drop after this move
 		
+		coveredGain +=calcVulnerability(randomNumber, countEmptyNeighbours(board, tile)); // how covered is the tile
+		
+		Tile[] neighbors = ProximityUtilities.getNeighbors(tile.getX(), tile.getY(), board);
 		for (Tile neighbor:neighbors) {
 			if ( neighbor == null) { // out of board
-				covered += tile.getScore();
 			}
 			else if ( neighbor.getPlayerId() ==  0 ) { // empty neighbor
-				enemy += getScoreEvaluation( board,  10, neighbor, pid);
-				enemy += (10 > randomNumber) ? randomNumber : 0;
 			}
-			else if ( neighbor.getPlayerId() == id ) { // my neighbor
-				neighborsScore += 1;
-				covered += neighbor.getScore();
+			else if ( neighbor.getPlayerId() == id ) { // friend neighbor
+				scoreGain += 1;
+				coveredGain += getVulnerabilityDiff(board, neighbor, 1, 1); // covered neighbor
 				
-				covered += tile.getScore();
 			}
 			else { // enemy neighbor
 				if ( randomNumber > neighbor.getScore() ) {
-					neighborsScore += neighbor.getScore();
-					covered += neighbor.getScore();
+					scoreGain += 2 * neighbor.getScore(); // what we get plus enemy loses
+					coveredGain += calcVulnerability(neighbor.getScore(),
+							countEmptyNeighbours(board, neighbor)); // (x2?)how covered is the new tile
 				}
-				covered += tile.getScore();
+				else {
+					// todo: if we don't conquer enemy, we cover him by one tile
+					// coveredGain -= getVulnerabilityDiff(board, neighbor, 0, 1); // covered
+				}
 			}
 		}
 		
-		System.out.println(covered / 3. + " " + neighborsScore);
-		return neighborsScore + enemyHere - enemy / 3;//+ (covered / 12.);
-		// points gained
-		// safety points gained
+		if (scoreGain > 0) System.out.println(tile.getX() + " " + tile.getY() + " " + randomNumber + ": " + scoreGain + " " + coveredGain);
+		return scoreGain + coveredGain;
 	}
 	
-	protected double getScoreEvaluation(Board board, int randomNumber, Tile tile, int pid) {
-		Tile[] neighbors = ProximityUtilities.getNeighbors(tile.getX(), tile.getY(), board);
-		int neighborsScore = 0;
+	/**
+	 * Calculates the drop of vulnerability after we add scoreAdded score 
+	 * and neighboursCovered neighbors to the tile
+	 * @param board
+	 * @param tile
+	 * @param scoreAdded
+	 * @param neighboursCovered
+	 * @return the vulnerability drop
+	 */
+	private double getVulnerabilityDiff(Board board, Tile tile, double scoreAdded, double neighboursCovered) {
+		double score = tile.getScore();
+		double emptyNeighbours = countEmptyNeighbours(board, tile);
 		
+		double newScore = score + scoreAdded;
+		double newEmptyNeighbours = emptyNeighbours - neighboursCovered;
+		
+		return calcVulnerability(score, emptyNeighbours) - calcVulnerability(newScore, newEmptyNeighbours);
+	}
+	
+	/**
+	 * Estimates how vulnerable a tile is to a neighbor's move next to it.
+	 * @param score The score of the tile
+	 * @param emptyNeighbours The number of empty neighbors of the tile.
+	 * @return vulnerability values between 0 and score
+	 */
+	private double calcVulnerability( double score, double emptyNeighbours ) {
+		// probability of enemy playing higher score tile 
+		// (can use getMyPool() here)
+		// values between 0 and 1
+		double enemyHigherCoef = (20 - score) / 20;
+		
+		// empty neighbors coefficient
+		// values between 0 and 1
+		double emptyNeighboursCoef = Math.pow(emptyNeighbours / 6, 0.4) ;
+		
+		return  enemyHigherCoef * emptyNeighboursCoef * score;
+	}
+	
+	/**
+	 * Counts the empty neighbors of the tile
+	 * @param board
+	 * @param tile
+	 * @return the number of empty neighbors
+	 */
+	private int countEmptyNeighbours(Board board, Tile tile) {
+		int emptyNeighbours = 0;
+		Tile[] neighbors = ProximityUtilities.getNeighbors(tile.getX(), tile.getY(), board);
 		for (Tile neighbor:neighbors) {
 			if ( neighbor == null) { // out of board
 			}
 			else if ( neighbor.getPlayerId() ==  0 ) { // empty neighbor
-			}
-			else if ( neighbor.getPlayerId() == pid ) { // my neighbor
-				neighborsScore += 1;
-			}
-			else { // enemy neighbor
-				if ( randomNumber > neighbor.getScore() ) {
-					neighborsScore += neighbor.getScore();
-				}
+				emptyNeighbours++;
 			}
 		}
-		
-		return neighborsScore;
+		return emptyNeighbours;
 	}
-
 }
-
 
