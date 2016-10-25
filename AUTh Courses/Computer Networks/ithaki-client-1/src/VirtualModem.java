b4e5312402ed48c8c85fb79374263b665a206621
Evangelos Mamalakis <mamalakis@auth.gr>
2016-10-25 04:28:24 +0300

    ithaki1 generalize to getResponse()

--- AUTh Courses/Computer Networks/ithaki-client-1/src/VirtualModem.java
+++ AUTh Courses/Computer Networks/ithaki-client-1/src/VirtualModem.java
@@ -1,48 +1,82 @@
 import ithakimodem.*;
 
 import java.io.*;
-import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 public class VirtualModem {
 	int speed = 10000;
 	int timeout = 2000;
 
 	String server_name        = "ithaki";
-	String echo_request_code  = "E9256\r";
-	String image_request_code = "M2373\r";
+	String echo_request_code  = "E2649\r";
+	String image_request_code = "M2516\r";
 	String gps_request_code   = "E4541\r";
+	long connection_timeout = 20000;
+	int[] start_image_delimiter = { 0xFF, 0xD8 };
+	int[] end_image_delimiter = { 0xFF, 0xD9 };
 	
     public static void main(String[] args) {
     	new VirtualModem().demo();
     }
     
-    public void demo() throws IOException {
+    public void demo() {
     	Modem modem = new Modem();
     	modem.setSpeed(speed);
     	modem.setTimeout(timeout);
     	modem.open(server_name);
-    	echoRequest(modem);
-    	imageRequest(modem);
+    	
+    	//echoRequest(modem);
+    	//imageRequest(modem);
+    	
+    	modem.write(echo_request_code.getBytes());
+    	modem.write(image_request_code.getBytes());
+    	receiveResponse(modem);
+    	
     	modem.close();
     }
     // NOTE : Break endless loop by catching sequence "\r\n\n\n".
     // NOTE : Stop program execution when "NO CARRIER" is detected.
     // NOTE : A time-out option will enhance program behavior.
     // NOTE : Continue with further Java code.
-    private void echoRequest(Modem modem) {
-    	modem.write(echo_request_code.getBytes());
-//    	System.out.println(Arrays.toString(echo_request_code.getBytes())); 
-    	
+    private void receiveResponse(Modem modem) {
+    	System.out.println("#Receiving...");
     	int incoming_byte;
+    	long startTime = System.currentTimeMillis(), elapsedTime, lastActiveTime = startTime;
+    	int image_delimeter_index = 0;
     	while( true ) {
-    		try {
+    		try {	
+    			// Timeout timer
+	    		elapsedTime = System.currentTimeMillis() - lastActiveTime;
+	    		if ( elapsedTime > connection_timeout ) {
+	    	    	System.out.println("#Timeout reached");
+	    	    	break;
+	    		}
+	    		
+	    		// Read and print
 	    		incoming_byte = modem.read();
 	    		if ( incoming_byte == -1) {
-	    	    	System.out.println("#Received -1"); 
-	    			break;
+	    	    	System.out.println("#Received -1");
+	    	    	TimeUnit.SECONDS.sleep(1);
+	    			continue;
 	    		}
 	    		
 	    		System.out.print((char) incoming_byte);
+	    		
+
+	    		// Check for image
+	    		if ( start_image_delimiter[image_delimeter_index] == incoming_byte ) { // Candidate delimiter 
+	    			System.out.println("#candidate img"); 
+	    			image_delimeter_index++;
+	    		}
+	    		else { // Reset
+	    			image_delimeter_index = 0;
+	    		}
+	    		if ( image_delimeter_index == start_image_delimiter.length ) { //Confirmed delimiter
+	    			System.out.println("#confirmed img"); 
+	    			getImage(modem);
+	    		}
+	    		lastActiveTime = System.currentTimeMillis();
+	    		
     		}
     		catch (Exception e) {
     	    	System.out.println("#Exception while reading."); 
@@ -50,16 +84,23 @@ public class VirtualModem {
     		}
     	}	
     }
-
-    private void imageRequest(Modem modem) {
-    	modem.write(image_request_code.getBytes());
-//    	System.out.println(Arrays.toString(echo_request_code.getBytes())); 
-		
+    
+    private void getImage(Modem modem) {
+    	File img = null;
     	OutputStream image_file = null;
     	try {
-    		image_file = new FileOutputStream("img.jpg");
+        	for (int i = 0; img == null || img.exists(); i++) { //Calculate unique name
+        		img = new File("img" + i + ".jpg");
+        		System.out.println("img.jpg"+i); 
+        	}
+    		image_file = new FileOutputStream(img);
         	
-    		boolean receiving_image = false;
+    		// Write delimiter bytes
+    		for (int delimiter_byte : start_image_delimiter)
+    			image_file.write(delimiter_byte);
+    		
+    		// Get image bytes
+    		int image_delimeter_index = 0;
 	    	int incoming_byte;
 	    	while( true ) {
 	    		try {
@@ -69,10 +110,19 @@ public class VirtualModem {
 		    			break;
 		    		}
 		    		
-		    		if (incoming_byte == 0xFF)
-		    			receiving_image = true;
-		    		
 	    		    image_file.write(incoming_byte);
+		    		
+	    		    // Check for image end
+		    		if ( end_image_delimiter[image_delimeter_index] == incoming_byte ) { // Candidate delimiter 
+		    			image_delimeter_index++;
+		    		}
+		    		else { // Reset
+		    			image_delimeter_index = 0;
+		    		}
+		    		if ( image_delimeter_index == end_image_delimiter.length ) { //Confirmed delimiter
+		    			System.out.println("#confirmed img end"); 
+		    			break;
+		    		}
 	    		}
 	    		catch (Exception e) {
 	    	    	System.out.println("#Exception while reading."); 
@@ -95,8 +145,4 @@ public class VirtualModem {
 		}
 	}
     
-    private String getByteResponse() {
-		return echo_request_code;
-    	
-    }
 }
